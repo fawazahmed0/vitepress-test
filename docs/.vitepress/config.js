9fdd5ccf1daa09df8062aca644117adee14ea5e7
Fawaz Ahmed <fawazahmed0@hotmail.com>
2022-06-02 18:46:48 +0000

    test build

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,16 @@
+export default {
+    title: 'Hello VitePress',
+    description: 'Just playing around.',
+    themeConfig: {
+        sidebar: [
+          {
+            text: 'Guide',
+            items: [
+              { text: 'Introduction', link: '/index' },
+              { text: 'Getting Started', link: '/getting-started' },
+
+            ]
+          }
+        ]
+      }
+  }
